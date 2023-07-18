3810f4c6104117d6acb5ae5ea24aea4bd45a6c1a
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2023-07-18 23:44:30 +0200

    afd

--- markdown-linkify/src/transform/mod.rs
+++ markdown-linkify/src/transform/mod.rs
@@ -1,8 +1,11 @@
 use std::fmt::Debug;
 
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 
 use crate::LinkMetadata;
+
+use self::substitution::Substitution;
 
 pub mod docs_rustlang_replacer;
 pub mod docsrs_replacer;
@@ -11,4 +14,17 @@ pub mod substitution;
 pub trait LinkTransformer: Debug {
     fn apply(&self, metadata: &mut LinkMetadata) -> anyhow::Result<()>;
     fn pattern(&self) -> Regex;
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Transformers {
+    pub regexes: Vec<Substitution>,
+}
+
+impl Transformers {
+    pub fn example() -> Self {
+        Self {
+            regexes: vec![Substitution::example(), Substitution::example()],
+        }
+    }
 }
